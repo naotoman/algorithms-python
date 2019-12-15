35480323bd63778fa32cbdab5ab7a0dd96c17449
Kido <sample@email.com>
2019-12-15 21:20:54 +0900

    add functions for comb

--- numbers_func.py
+++ numbers_func.py
@@ -1,3 +1,4 @@
+# 素因数分解
 def prime_fact_dic(n):
     """Returns the dictionary that contains the prime factors of integer `n`.
 
@@ -7,18 +8,18 @@ def prime_fact_dic(n):
     Returns:
       dict: The dictionary whose keys are the prime factors of `n` and values are the number of those appearances.
     """
-    facts = {}
+    factors = {}
     d = 2
     while d * d <= n:
         while n % d == 0:
-            facts[d] = facts.get(d, 0) + 1
+            factors[d] = factors.get(d, 0) + 1
             n //= d
         d += 1
     if n > 1:
-        facts[n] = 1
-    return facts
+        factors[n] = 1
+    return factors
 
-
+# 素因数分解
 def prime_fact_list(n):
     """Returns the list that contians the prime factors of integer `n`.
 
@@ -28,13 +29,53 @@ def prime_fact_list(n):
     Returns:
       list: The list that contains the all prime factors of `n`.
     """
-    facts = []
+    factors = []
     d = 2
     while d * d <= n:
         while n % d == 0:
-            facts.append(d)
+            factors.append(d)
             n //= d
         d += 1
     if n > 1:
-        facts.append(n)
-    return facts
+        factors.append(n)
+    return factors
+
+# #二項係数(mod)
+def comb(n, k, mod, fac, finv):
+    if n < k or n < 0 or k < 0:
+        return 0
+    return fac[n] * (finv[k] * finv[n - k] % mod) % mod
+
+# 階乗の逆元(mod)
+def factorials_inv_mod(n, mod):
+    inv = [1] * (n + 1)
+    factorials_inv = [1] * (n + 1)
+    for i in range(2, n + 1):
+        inv[i] = mod - inv[mod % i] * (mod // i) % mod
+        factorials_inv[i] = factorials_inv[i - 1] * inv[i] % mod
+    return factorials_inv
+
+# 階乗(mod)
+def factorials_mod(n, mod):
+    factorials = [1] * (n + 1)
+    for i in range(2, n+1):
+        factorials[i] = (factorials[i-1] * i) % mod
+    return factorials
+
+# 二分累乗法(mod) #べき乗 #冪乗
+def pow_mod(base, exp, mod):
+    res = 1
+    x = base
+    while exp > 0:
+        if exp & 1:
+            res = (res * x) % mod
+        x = (x ** 2) % mod
+        exp >>= 1
+    return res
+
+# 最大公約数 #ユークリットの互除法
+def gcd(a, b):
+    a, b = min(a, b), max(a, b)
+    while a > 0:
+        a, b = b % a, a
+    return b
--- tests/test_numbers_func.py
+++ tests/test_numbers_func.py
@@ -19,6 +19,7 @@ class TestNumbersFunc(unittest.TestCase):
             result = numbers_func.prime_fact_dic(x)
             self.assertEqual(result, expect)
 
+
     def test_prime_fact_list(self):
         testCase = {
             2: [2],
@@ -30,6 +31,42 @@ class TestNumbersFunc(unittest.TestCase):
         }
         for x, expect in testCase.items():
             result = numbers_func.prime_fact_list(x)
+            self.assertEqual(result, expect)
+
+
+    def test_pow_mod(self):
+        testCase = {
+            (1, 1, 100): 1,
+            (1, 8, 100): 1,
+            (2, 1, 100): 2,
+            (10, 1, 5): 0,
+            (10, 1, 7): 3,
+            (10, 1, 100): 10,
+            (2, 7, 100): 28,
+            (3, 5, 5): 3,
+            (3, 5, 2): 1,
+            (3, 5, 10): 3,
+            (3, 5, 9): 0,
+        }
+        for (x, y, z), expect in testCase.items():
+            result = numbers_func.pow_mod(x, y, z)
+            self.assertEqual(result, expect)
+
+
+    def test_gcd(self):
+        testCase = {
+            (1, 1): 1,
+            (1, 2): 1,
+            (2, 1): 1,
+            (3, 3): 3,
+            (4, 4): 4,
+            (6, 35): 1,
+            (15, 12): 3,
+            (51, 23): 1,
+            (6, 150): 6,
+        }
+        for (x, y), expect in testCase.items():
+            result = numbers_func.gcd(x, y)
             self.assertEqual(result, expect)
 
 if __name__ == '__main__':
